3d7a98d723264b98982510a76592e342ccd5d45a
Kevin P. Gyorick <kpgyoric@csis.svsu.edu>
2020-02-22 10:52:34 -0500

    new home page

--- index.php
+++ index.php
@@ -1,18 +1,53 @@
 <html>
     <head>
-        
+        <style>
+                html {
+                    background-color: #202225;
+                    height: 100%;
+                    width: 100%;
+                }
+
+                body {
+                    background-color: #36393F;
+                    min-height: 60%;
+                    max-width: 1000px;
+                    display: block;
+                    margin: 0 auto;
+                    padding: 10% 0;
+                    text-align: center;
+                }
+
+                p, h1, h2 {
+                    color: white;
+                }
+
+                h1 {
+                    font-size: 3vw;
+                }
+
+                h2 {
+                    font-size: 2vw;
+                }
+
+                p {
+                    font-size: 1.2vw;
+                }
+
+                a {
+                    color: rgb(207, 207, 207);
+                    text-decoration: none;
+                }
+            </style>
     </head>
     
     <body>
-        <h1>Assignments</h1>
-        <ul>
-            <li>AS01: <a href="AS01/helloworld.php">Hello World</a>, <a href="AS01/showarrays.php">Array</a> and <a href="AS01/banas.php">Banas</a></li>
-            <li>AS02: <a href="AS02/display_list.php">CRUD</a></li>
-            <li>AS03: <a href="AS03/as03.php">Customer and Events Tables</a></li>
-            <li>AS04: <a href="AS04/assignments.php">Assignments Table</a></li>
-            <li>Source Code: <a href="https://github.com/everythingguy/cis355">Github</a></li>
-            <br>
-            <li>CIS 255: <a href="http://hw.duraken.com">here</a></li>
-        </ul>
+        <h1>Kevin Gyorick's Assignments</h1>
+        <p>AS01: <a href="AS01/helloworld.php">Hello World</a>, <a href="AS01/showarrays.php">Array</a> and <a href="AS01/banas.php">Banas</a></p>
+        <p>AS02: <a href="AS02/display_list.php">CRUD</a></p>
+        <p>AS03: <a href="AS03/as03.php">Customer and Events Tables</a></p>
+        <p>AS04: <a href="AS04/assignments.php">Assignments Table</a></p>
+        <p>Source Code: <a href="https://github.com/everythingguy/cis355">Github</a></p>
+        <br>
+        <p>CIS 255: <a href="http://hw.duraken.com">here</a></p>
     </body>
 </html>
